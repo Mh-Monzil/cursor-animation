47c29b05f4a5c13de814eda13c0d7a5f82195c00
Mh-Monzil <monzil246@gmail.com>
2024-07-26 02:18:40 +0600

    image hover custom cursor

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 var main = document.querySelector("#main")
 var cursor = document.querySelector("#cursor")
-
+var imageDiv = document.querySelector("#image")
 
 main.addEventListener("mousemove", (val) => {
-    console.log("click");
     gsap.to(cursor, {
         x: val.x-8,
         y: val.y-6,
-        duration: 1.2,
-        ease: "back.out"
+        duration: 0.5,
+    })
+})
+
+
+imageDiv.addEventListener("mouseenter", () => {
+    cursor.innerHTML = "View More"
+    gsap.to(cursor, {
+        scale:3,
+        backgroundColor:"#ffffff88",
+    })
+})
+
+imageDiv.addEventListener("mouseleave", () => {
+    cursor.innerHTML = ""
+    gsap.to(cursor, {
+        scale:1,
+        backgroundColor: "",
     })
 })
